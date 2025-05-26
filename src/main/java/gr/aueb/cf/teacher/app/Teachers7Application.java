d04b7afad59b8c8beac308f97e5559e68a993c65
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-26 14:04:27 +0300

    feat: jpaauditing for timestamps

--- src/main/java/gr/aueb/cf/teacher/app/Teachers7Application.java
+++ src/main/java/gr/aueb/cf/teacher/app/Teachers7Application.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.teacher.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Teachers7Application {
 
 	public static void main(String[] args) {

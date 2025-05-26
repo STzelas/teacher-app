26c8254597ddd8a6cc8ce3c0e67d41ae835b6972
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-26 14:01:44 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/teacher/app/Teachers7Application.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.teacher.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teachers7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teachers7Application.class, args);
+	}
+
+}

5e73bf4fefcea6a0da48c065bc86e4676bece8b7
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-30 18:32:32 +0300

    feat: teacher and region api added

---
+++ src/main/java/gr/aueb/cf/teacher/teacherapp/service/IRegionService.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.teacher.teacherapp.service;
+
+import gr.aueb.cf.teacher.teacherapp.model.static_data.Region;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IRegionService {
+    List<Region> findAllRegions();
+}
---
+++ src/main/java/gr/aueb/cf/teacher/teacherapp/service/ITeacherService.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.teacher.teacherapp.service;
+
+import gr.aueb.cf.teacher.teacherapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.teacher.teacherapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.teacher.teacherapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.teacher.teacherapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.teacher.teacherapp.model.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ITeacherService {
+
+    Teacher saveTeacher(TeacherInsertDTO dto)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(int page, int size);
+}

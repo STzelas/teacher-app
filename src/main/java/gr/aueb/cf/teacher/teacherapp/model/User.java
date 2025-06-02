5fc5f08bdd2809dca43e3b4ecf5fdd82f3311d00
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-02 13:22:56 +0300

    feat: user entity completed

--- src/main/java/gr/aueb/cf/teacher/teacherapp/model/User.java
+++ src/main/java/gr/aueb/cf/teacher/teacherapp/model/User.java
@@ -37,32 +37,22 @@ public class User extends AbstractEntity implements UserDetails {
     }
 
     @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }

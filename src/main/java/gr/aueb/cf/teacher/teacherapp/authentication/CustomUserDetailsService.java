03d9c62d25e23f5999ffb5ace323269a259edc36
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-02 13:41:40 +0300

    feat: custom user details service added

---
+++ src/main/java/gr/aueb/cf/teacher/teacherapp/authentication/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.teacher.teacherapp.authentication;
+
+import gr.aueb.cf.teacher.teacherapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username: " + username + " not found"));
+    }
+}

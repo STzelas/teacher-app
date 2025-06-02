0971ea7bf5790231646348c9d691faf5a37d2075
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-02 20:56:13 +0300

    feat: map to user entity added

--- src/main/java/gr/aueb/cf/teacher/teacherapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/teacher/teacherapp/mapper/Mapper.java
@@ -1,8 +1,11 @@
 package gr.aueb.cf.teacher.teacherapp.mapper;
 
+import gr.aueb.cf.teacher.teacherapp.core.enums.Role;
 import gr.aueb.cf.teacher.teacherapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.teacher.teacherapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.teacher.teacherapp.dto.UserInsertDTO;
 import gr.aueb.cf.teacher.teacherapp.model.Teacher;
+import gr.aueb.cf.teacher.teacherapp.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,5 +24,9 @@ public class Mapper {
         return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreated_at(),
                 teacher.getUpdated_at(), teacher.getUuid(), teacher.getFirstname(),
                 teacher.getLastname(), teacher.getVat(), teacher.getRegion().getName());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(null, userInsertDTO.getUsername(), userInsertDTO.getPassword(), Role.valueOf(userInsertDTO.getRole().toUpperCase()));
     }
 }
